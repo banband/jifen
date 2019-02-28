80f16d3a7595ac1391053536e1f93b43c9717b8e
banband <393377477@qq.com>
2019-02-28 11:58:19 +0800

    差 打开关闭主控和输入非法字符

--- zhidong/45646.py
+++ zhidong/45646.py
@@ -1,14 +1,7 @@
 import re
-zuolibiaoding = open("平板线\Data\标定数据\左力标定.txt",'w')
-file_context = zuolibiaoding.readlines()  # file_context是一个string，读取完后，就失去了对test.txt的文件引用
-for i in file_context:
-    d = re.findall(r";.*?;(.+?)\n", i)
-
-    d = d[0]
-    x = float(d)
-    x = x * 100
-    print(d)
-    print(x)
-
-zuolibiaoding.close()
-#print(file_context)
+import os
+beifenweizhi = "Data\标定数据"
+a = os.path.exists("11")
+print(a)
+if a == False:
+    os.mkdir(beifenweizhi+"\\标定备份")
--- zhidong/shiyan.py
+++ zhidong/shiyan.py
@@ -1,11 +1,18 @@
 import re
 import random
 import configparser
+import os
 
+biaodingweizhi = "Data\标定数据\\"
+beifenweizhi = "Data\标定数据\标定备份\\"
+jianchalujing = "Data\标定数据\\"
 class beifen(object):
     def chuangjian(self,lujing,newlujing):
         self.lujing = lujing
         self.newlujing = newlujing
+        a = os.path.exists(jianchalujing+"标定备份")
+        if a == False:
+            os.mkdir(jianchalujing + "标定备份")
         with open(self.lujing,"r",encoding="utf-8") as q_s:
             lines = q_s.readlines()
         with open(self.newlujing, "w", encoding="utf-8") as j_w:
@@ -31,15 +38,16 @@ class jisuan(object):
             lines = f.readlines()
         with open(self.lujing,"w",encoding="utf-8") as f_w:
             for i in lines:
-                d = re.findall(r";.*?;(.*?)\n", i)
-                e = float(d[0])
-                d = d[0]+'\n'
+                d = re.split(r";",i)
+                e = float(d[2])
+                print(e)
                 x = (e / int(self.baifenbi)) * int(self.libuchang) * 0.8
-                x = str('%.1f'%x)+'\n'
-                line = i.replace(d,x,1)
+                x = str('%.1f'%x)
+                line = i.replace(str(e),x)
                 f_w.write(line)
 
 if __name__ == '__main__':
+
     config = configparser.ConfigParser(allow_no_value=True)
     config.read("参数配置.ini")
     zuolitongdao = config.get("参数", "左力通道")
@@ -48,16 +56,79 @@ if __name__ == '__main__':
     pingbanqianzuolitongdao = config.get("参数", "平板前左力通道")
     pingbanqianyoulitongdao = config.get("参数", "平板前右力通道")
     pingbanhouzuolitongdao = config.get("参数", "平板后左力通道")
-    pingbanhuoyoulitongdao = config.get("参数", "平板后右力通道")
+    pingbanhouyoulitongdao = config.get("参数", "平板后右力通道")
     if jiancexianleixing == "1":
         print("这是一套平板线")
-        pass
+        beifen_1 = beifen()
+        beifen_1.chuangjian(biaodingweizhi+"平板前左力标定.txt", beifenweizhi+"平板前左力标定备份.txt")
+        beifen_1.chuangjian(biaodingweizhi+"平板前右力标定.txt", beifenweizhi+"平板前右力标定备份.txt")
+        beifen_1.chuangjian(biaodingweizhi+"平板后左力标定.txt", beifenweizhi+"平板后左力标定备份.txt")
+        beifen_1.chuangjian(biaodingweizhi+"平板后右力标定.txt", beifenweizhi+"平板后右力标定备份.txt")
         while True:
-            pass
+            if pingbanqianzuolitongdao == pingbanqianyoulitongdao and pingbanhouzuolitongdao == pingbanhouyoulitongdao:
+                print("左右轮并行")
+                zhidonglvq = input("请输入前轴制动力（回车默认恢复默认）：")
+                zhidonglvh = input("请输入后轴制动力（回车默认恢复默认）：")
+                if zhidonglvq == "" or zhidonglvh == "":
+                    chongzhi = gongju()
+                    chongzhi.xieruyuanshi(biaodingweizhi+"平板前左力标定.txt", beifenweizhi+"平板前左力标定备份.txt")
+                    chongzhi.xieruyuanshi(biaodingweizhi+"平板前右力标定.txt", beifenweizhi+"平板前右力标定备份.txt")
+                    chongzhi.xieruyuanshi(biaodingweizhi+"平板后左力标定.txt", beifenweizhi+"平板后左力标定备份.txt")
+                    chongzhi.xieruyuanshi(biaodingweizhi+"平板后右力标定.txt", beifenweizhi+"平板后右力标定备份.txt")
+                else:
+                    chazhidian = 100
+                    zhidonglvq = int(zhidonglvq)
+                    zhidonglvh = int(zhidonglvh)
+                    baifenbi = jisuan()
+                    baifenbi.xierubianliang(biaodingweizhi+"平板前左力标定.txt", zhidonglvq, chazhidian)
+                    baifenbi.xierubianliang(biaodingweizhi+"平板前右力标定.txt", zhidonglvq, chazhidian)
+                    baifenbi.xierubianliang(biaodingweizhi+"平板后左力标定.txt", zhidonglvh, chazhidian)
+                    baifenbi.xierubianliang(biaodingweizhi+"平板后右力标定.txt", zhidonglvh, chazhidian)
+
+            else:
+                print("正常线路")
+                zhidonglvq = input("请输入前轴制动力（回车默认恢复默认）：")
+                zhidonglvh = input("请输入后轴制动力（回车默认恢复默认）：")
+                if zhidonglvq == "" or zhidonglvh == "":
+                    chongzhi = gongju()
+                    chongzhi.xieruyuanshi(biaodingweizhi+"平板前左力标定.txt", beifenweizhi+"平板前左力标定备份.txt")
+                    chongzhi.xieruyuanshi(biaodingweizhi+"平板前右力标定.txt", beifenweizhi+"平板前右力标定备份.txt")
+                    chongzhi.xieruyuanshi(biaodingweizhi+"平板后左力标定.txt", beifenweizhi+"平板后左力标定备份.txt")
+                    chongzhi.xieruyuanshi(biaodingweizhi+"平板后右力标定.txt", beifenweizhi+"平板后右力标定备份.txt")
+                else:
+                    chazhidianq = input("请输入前轴差值点（回车默认0%,其他（包括输入0）正负随机9）：")
+                    chazhidianh = input("请输入后轴差值点（回车默认0%,其他（包括输入0）正负随机9）：")
+                    if chazhidianq == "":
+                        chazhidianq = 100
+                        baifenbi = jisuan()
+                        baifenbi.xierubianliang(biaodingweizhi+"平板前左力标定.txt", zhidonglvq, chazhidianq)
+                        baifenbi.xierubianliang(biaodingweizhi+"平板前右力标定.txt", zhidonglvq, chazhidianq)
+                    else:
+                        chazhidianq = int(chazhidianq) + random.randint(-9, 9)
+                        zuolibuchang = 100 - chazhidianq
+                        youlibuchang = 100 + chazhidianq
+                        baifenbi = jisuan()
+                        baifenbi.xierubianliang(biaodingweizhi+"平板前左力标定.txt", zhidonglvq, zuolibuchang)
+                        baifenbi.xierubianliang(biaodingweizhi+"平板前右力标定.txt", zhidonglvq, youlibuchang)
+                    if chazhidianh == "":
+                        chazhidianh = 100
+                        baifenbi = jisuan()
+                        baifenbi.xierubianliang(biaodingweizhi+"平板后左力标定.txt", zhidonglvh, chazhidianh)
+                        baifenbi.xierubianliang(biaodingweizhi+"平板后右力标定.txt", zhidonglvh, chazhidianh)
+                    else:
+                        chazhidianh = int(chazhidianh) + random.randint(-9, 9)
+                        zuolibuchang = 100 - chazhidianh
+                        youlibuchang = 100 + chazhidianh
+                        baifenbi = jisuan()
+                        baifenbi.xierubianliang(biaodingweizhi+"平板后左力标定.txt", zhidonglvh, zuolibuchang)
+                        baifenbi.xierubianliang(biaodingweizhi+"平板后右力标定.txt", zhidonglvh, youlibuchang)
+
+
+
     if jiancexianleixing == "0":
         beifen_1 = beifen()
-        beifen_1.chuangjian("左力标定.txt","左力标定备份.txt")
-        beifen_1.chuangjian("右力标定.txt","右力标定备份.txt")
+        beifen_1.chuangjian(biaodingweizhi+"左力标定.txt",beifenweizhi+"左力标定备份.txt")
+        beifen_1.chuangjian(biaodingweizhi+"右力标定.txt",beifenweizhi+"右力标定备份.txt")
         print("这是一套滚筒线")
         while True:
             if zuolitongdao == youlitongdao:
@@ -65,35 +136,35 @@ if __name__ == '__main__':
                 zhidonglv = input("请输入制动力（回车默认恢复默认）：")
                 if zhidonglv == "":
                     chongzhi = gongju()
-                    chongzhi.xieruyuanshi("左力标定.txt","左力标定备份.txt")
-                    chongzhi.xieruyuanshi("右力标定.txt","右力标定备份.txt")
+                    chongzhi.xieruyuanshi(biaodingweizhi+"左力标定.txt",beifenweizhi+"左力标定备份.txt")
+                    chongzhi.xieruyuanshi(biaodingweizhi+"右力标定.txt",beifenweizhi+"右力标定备份.txt")
                 else:
                     chazhidian = 100
                     zhidonglv = int(zhidonglv)
                     baifenbi = jisuan()
-                    baifenbi.xierubianliang("左力标定.txt", zhidonglv, chazhidian)
-                    baifenbi.xierubianliang("右力标定.txt", zhidonglv, chazhidian)
+                    baifenbi.xierubianliang(biaodingweizhi+"左力标定.txt", zhidonglv, chazhidian)
+                    baifenbi.xierubianliang(biaodingweizhi+"右力标定.txt", zhidonglv, chazhidian)
 
             else:
                 print("正常线路")
                 zhidonglv = input("请输入制动力（回车默认恢复默认）：")
                 if zhidonglv == "":
                     chongzhi = gongju()
-                    chongzhi.xieruyuanshi("左力标定.txt", "左力标定备份.txt")
-                    chongzhi.xieruyuanshi("右力标定.txt", "右力标定备份.txt")
+                    chongzhi.xieruyuanshi(biaodingweizhi+"左力标定.txt", beifenweizhi+"左力标定备份.txt")
+                    chongzhi.xieruyuanshi(biaodingweizhi+"右力标定.txt", beifenweizhi+"右力标定备份.txt")
                 else:
                     chazhidian = input("请输入差值点（回车默认0%,其他（包括输入0）正负随机9）：")
                     if chazhidian == "":
                         chazhidian = 100
                         baifenbi = jisuan()
-                        baifenbi.xierubianliang("左力标定.txt", zhidonglv, chazhidian)
-                        baifenbi.xierubianliang("右力标定.txt", zhidonglv, chazhidian)
+                        baifenbi.xierubianliang(biaodingweizhi+"左力标定.txt", zhidonglv, chazhidian)
+                        baifenbi.xierubianliang(biaodingweizhi+"右力标定.txt", zhidonglv, chazhidian)
                     else:
                         chazhidian = int(chazhidian) + random.randint(-9, 9)
                         zuolibuchang = 100 - chazhidian
                         youlibuchang = 100 + chazhidian
                         baifenbi = jisuan()
-                        baifenbi.xierubianliang("左力标定.txt", zhidonglv, zuolibuchang)
-                        baifenbi.xierubianliang("右力标定.txt", zhidonglv, youlibuchang)
+                        baifenbi.xierubianliang(biaodingweizhi+"左力标定.txt", zhidonglv, zuolibuchang)
+                        baifenbi.xierubianliang(biaodingweizhi+"右力标定.txt", zhidonglv, youlibuchang)
 
 
