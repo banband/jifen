eab934447aa6f92e96503c4e3baf57b38e2c2bba
banband <393377477@qq.com>
2019-02-28 16:02:29 +0800

    基本全部完成

--- zhidong/45646.py
+++ zhidong/45646.py
@@ -1,7 +1,20 @@
 import re
+
+import sys
 import os
-beifenweizhi = "Data\标定数据"
-a = os.path.exists("11")
-print(a)
-if a == False:
-    os.mkdir(beifenweizhi+"\\标定备份")
+import time
+import subprocess
+
+dangqianweizhi = __file__
+
+if dangqianweizhi[0] not in ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]:
+    print("部是")
+
+
+
+subprocess.Popen(r'滚筒线\PJCXMain.exe')
+print('打开成功')
+time.sleep(5)
+print('休息5s')
+os.system(r'taskkill /F /IM PJCXMain.exe')
+print('关闭成功')
--- zhidong/shiyan.py
+++ zhidong/shiyan.py
@@ -2,10 +2,20 @@ import re
 import random
 import configparser
 import os
+import time
+import subprocess
 
 biaodingweizhi = "Data\标定数据\\"
 beifenweizhi = "Data\标定数据\标定备份\\"
 jianchalujing = "Data\标定数据\\"
+dangqianweizhi = ""
+
+class congkai(object):
+    def guanbidakai(self):
+        os.system(r'taskkill /F /IM PJCXMain.exe')
+        time.sleep(2)
+        subprocess.Popen(r'PJCXMain.exe')
+
 class beifen(object):
     def chuangjian(self,lujing,newlujing):
         self.lujing = lujing
@@ -47,6 +57,24 @@ class jisuan(object):
                 f_w.write(line)
 
 if __name__ == '__main__':
+    dangqianweizhi = __file__
+    print("使用说明：\n"
+          "1、本软件需放在检测系统根目录下运行，自动识别《参数配置.ini》文件下参数识别检测线类型，\n"
+          "《参数配置.ini》下要求[参数]和标定数据文件夹下都需含有左力通道、右力通道、平板前左力通道、平板前右力通道、平板后左力通道、平板后右力通道。\n"
+          "《参数配置.ini》[工位设定]下要求含有平板= 项目，《参数配置.ini》下每个子项目下不能含有重名字典。如不符合可能造成软件崩溃。\n"
+          "2、本软件根据运行环境自动选择是否开启自动关闭、打开主控功能。\n"
+          "3、本软件可以自动识别滚筒线、平板线、左右轮是否通道相同，根据识别后的参数自动选择运行模式。\n"
+          "4、本软件会在打开的同时在标定数据文件夹下创建标定数据备份，请在关闭软件之前空按回车将备份还原，\n"
+          "软件每次打开都会覆盖备份，请将主控程序备份妥善保管，以免因为不当的操作或此软件BUG造成文件丢失。\n"
+          "5、此软件无法运行在win7系统以下运行，如果要在win7以下运行请选择“曲线救国模式：工位机开启文件夹共享，将此软件在win7以上系统上远程执行。”")
+    input("6、确认已经认真阅读以上4条，进入软件请按回车键，如造成崩溃，请按照以上4条修改参数配置。")
+    if dangqianweizhi[0] in ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+                                 "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]:
+        dangqianweizhi = 1
+        print("*******软件运行在本机环境，已开启自动关闭主控、打开主控功能！")
+    else:
+        dangqianweizhi = 0
+        print("*******软件运行在共享文件夹环境，已关闭自动关闭主控、打开主控功能！")
 
     config = configparser.ConfigParser(allow_no_value=True)
     config.read("参数配置.ini")
@@ -123,7 +151,9 @@ if __name__ == '__main__':
                         baifenbi.xierubianliang(biaodingweizhi+"平板后左力标定.txt", zhidonglvh, zuolibuchang)
                         baifenbi.xierubianliang(biaodingweizhi+"平板后右力标定.txt", zhidonglvh, youlibuchang)
 
-
+            if dangqianweizhi == 1:
+                q = congkai()
+                q.guanbidakai()
 
     if jiancexianleixing == "0":
         beifen_1 = beifen()
@@ -166,5 +196,9 @@ if __name__ == '__main__':
                         baifenbi = jisuan()
                         baifenbi.xierubianliang(biaodingweizhi+"左力标定.txt", zhidonglv, zuolibuchang)
                         baifenbi.xierubianliang(biaodingweizhi+"右力标定.txt", zhidonglv, youlibuchang)
+            if dangqianweizhi == 1:
+                q = congkai()
+                q.guanbidakai()
+
 
 
